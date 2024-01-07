a7370b51c93d6ee41e0bbaa87ef5e2eb9ead8118
medkhabt <medkha.bt@gmail.com>
2024-01-07 01:43:07 -0800

    shared memory left and some edge cases for the others

---
+++ 2_question/generate_stats.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <string.h>
+#include "generate_stats.h"
+void generateStats(const int nb_core, const int N, const int M, const double &runtime, const size_t nsplits) {
+       //	struct stat buffer; 
+	//bool file_exists = stat ( "stats.csv", &buffer ) != 0; 
+	std::ofstream stats; 
+	stats.open("stats.csv", std::ios_base::app); 
+	//if(file_exists){
+	//	stats << " nbcore, num_steps, runtime" << std::endl; 
+	//}
+	// columns names 
+	// values 
+	    stats  << nb_core << "," <<  log2(N) << "," << log2(M) << "," << runtime <<  std::endl;
+
+	
+	stats.close();
+}
